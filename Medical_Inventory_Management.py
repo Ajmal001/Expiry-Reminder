b7bfd692cfdcd865f86b039b3f307aea30cedbd2
Sadhgun Sobti <48918876+Sadhgun@users.noreply.github.com>
2019-06-11 00:06:12 +0530

    Add files via upload

--- Medical_Inventory_Management.py
+++ Medical_Inventory_Management.py
@@ -5,6 +5,33 @@ import os
 import time
 from datetime import datetime
 from tkinter.font import Font
+import configparser
+
+
+def get_setting(config, section):
+    #Returns a value
+    value = config.get(section, "types")
+    return value
+
+
+def Get_Types(config, group):
+    types = get_setting(config, group).split(", ")
+    print(types)
+
+
+def Get_Groups():
+    config = configparser.ConfigParser()
+    path = "Config.ini"
+    config.read(path)
+    groups = config.sections()
+    for group in groups:
+        print(group)
+        Get_Types(config, group)
+
+def Add_Groups():
+    global add_groups_screen
+
+    add_groups_screen = tk.Frame(master)
 
 
 def Add():
@@ -25,6 +52,7 @@ def Add():
     else:
         popupmsg("Fill in all the details")
     log.close()
+
 
 def Delete():
     nam = str(del_name.get())
@@ -49,6 +77,7 @@ def Delete():
         os.rename("temp.db", "inventory.db")
     else:
         popupmsg("Fill in all the details")
+
 
 def Del_sel():
     nam = str(del_name.get())
@@ -87,7 +116,7 @@ def Del_sel():
 
 
 def popupmsg(msg):
-    popup = tk.Tk()
+    popup = tk.Toplevel(master)
     popup.wm_title("!")
     label = tk.Label(popup, text=msg)
     label.pack(side="top", fill="x", pady=10)
@@ -96,43 +125,60 @@ def popupmsg(msg):
     popup.mainloop()
 
 
+def Add_Inventory():
+    global add_inventory_screen
+    global name
+    global type
+    global cal
+    global qty
+    global visible
+
+    add_inventory_screen = tk.Frame(master, bg = "white")
+
+    name = tk.StringVar(add_inventory_screen)
+    tkvar = tk.StringVar(add_inventory_screen)
+    choices = { 'Syringes','Medicine'}
+    tkvar.set('Medicine')
+    qty = tk.StringVar(add_inventory_screen)
+
+    tk.Label(add_inventory_screen, text="Add to Inventory", bg="white", font=("Calibri", 25), borderwidth=5, relief="solid").place(x=120,y=10,width=1126,height=60)
+
+    tk.Label(add_inventory_screen, text="Groups", bg="white", font=("Calibri", 25)).place(x=160,y=75,width=120,height=50)
+    tk.Label(add_inventory_screen, text="Groups", bg="white", borderwidth=5, relief="solid").place(x=40,y=120,width=400,height=550)
+
+    tk.Label(add_inventory_screen, text="Types", bg="white", font=("Calibri", 25)).place(x=620,y=75,width=100,height=50)
+    tk.Label(add_inventory_screen, text="Types", bg="white", borderwidth=5, relief="solid").place(x=490,y=120,width=400,height=550)
+
+    tk.Label(add_inventory_screen, text="Name", bg="white").place(x=950, y=310)
+    tk.Entry(add_inventory_screen, textvariable=name,width=30).place(x=1050, y=310)
+
+    tk.Label(add_inventory_screen, text="Date of Expiry", bg="white").place(x=950, y=340)
+    cal = DateEntry(add_inventory_screen, width=29, background='black', foreground='white', borderwidth=2)
+    cal.place(x=1050, y=340)
+
+    tk.Label(add_inventory_screen, text="Quantity", bg="white").place(x=950, y=370)
+    tk.Entry(add_inventory_screen, textvariable=qty,width=30).place(x=1050, y=370)
+
+    tk.Button(add_inventory_screen, text = 'Save to file', command=Add).place(x=1075, y=400)
+
+    visible = add_inventory_screen
+    add_inventory_screen.place(x=0, y=0, width=1366, height=768)
+
+
 master = tk.Tk()
 master.title("Inventory Management")
-master.geometry("320x200")
+master.geometry("1366x768")
 
+#Add_Inventory()
 
-tk.Label(master, text="   ").grid(row=0,column=0)
-name = tk.StringVar(master)
-tk.Label(master, text="Name").grid(row=0,column=1)
-tk.Entry(master, textvariable=name,width=39).grid(row=0,column=2)
-
-
-tkvar = tk.StringVar(master)
-choices = { 'Syringes','Medicine'}
-tkvar.set('Medicine')
-popupMenu = tk.OptionMenu(master, tkvar, *choices).grid(row = 1, column =2)
-tk.Label(master, text="Type").grid(row = 1, column = 1)
-
-
-tk.Label(master, text="Date").grid(row=2,column=1)
-cal = DateEntry(master, width=36, background='black', foreground='white', borderwidth=2)
-cal.grid(row=2,column=2)
-
-qty = tk.StringVar(master)
-tk.Label(master, text="Quantity").grid(row=3,column=1)
-tk.Entry(master, textvariable=qty,width=39).grid(row=3,column=2)
-
-tk.Button(master, text = 'Save to file', command=Add).grid(row=4,column=2)
-
-del_name = tk.StringVar(master)
-tk.Label(master, text="Name").grid(row=5,column=1)
-tk.Entry(master, textvariable=del_name,width=39).grid(row=5,column=2)
-
-del_qty = tk.StringVar(master)
-tk.Label(master, text="Quantity").grid(row=6,column=1)
-tk.Entry(master, textvariable=del_qty,width=39).grid(row=6,column=2)
-
-tk.Button(master, text = 'Delete', command=Delete).grid(row=7,column=1)
-
-tk.Button(master, text = 'Quantity used up', command=Del_sel).grid(row=7,column=2)
-master.mainloop()
+menubar = tk.Menu(master)
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label="Add Group")
+filemenu.add_command(label="Edit Group")
+filemenu.add_command(label="Delete Group")
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=master.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+master.config(menu=menubar)
+#master.mainloop()
+Get_Groups()
