bb4c21af72e53dd22e143c75c8a99104b07a2d57
Sadhgun Sobti <48918876+Sadhgun@users.noreply.github.com>
2019-06-11 20:27:31 +0530

    Add files via upload

--- Medical_Inventory_Management.py
+++ Medical_Inventory_Management.py
@@ -8,15 +8,79 @@ from tkinter.font import Font
 import configparser
 
 
+def toAdd_Inventory():
+    visible.place_forget()
+    Add_Inventory()
+
+
+def toModify_Groups():
+    visible.place_forget()
+    Modify_Groups()
+
+
+def Modify_Groups():
+    global modify_groups_screen
+    global place_holder
+    global visible
+    global new_grp
+    global new_type
+
+    new_grp = tk.StringVar()
+    new_type = tk.StringVar()
+
+    modify_groups_screen = tk.Frame(master, bg="white")
+
+    tk.Label(modify_groups_screen, text="Modify Groups", bg="white", font=("Calibri", 25), borderwidth=5, relief="solid").place(x=120,y=10,width=1126,height=60)
+
+    tk.Label(modify_groups_screen, text="Groups", bg="white", font=("Calibri", 25)).place(x=160,y=75,width=120,height=50)
+    groupsbox = tk.Listbox(modify_groups_screen)
+    groups = Get_Groups()
+    for group in groups:
+        groupsbox.insert(tk.END, group)
+    groupsbox.bind("<Double-Button-1>", Get_Types)
+    groupsbox.place(x=40,y=120,width=400,height=550)
+
+    tk.Label(modify_groups_screen, text="Types", bg="white", font=("Calibri", 25)).place(x=620,y=75,width=100,height=50)
+    place_holder = tk.Label(modify_groups_screen, text="Please Choose a group first.", bg="white", borderwidth=5, relief="solid")
+    place_holder.place(x=490,y=120,width=400,height=550)
+
+    tk.Label(modify_groups_screen, text="Add New Group", bg="white").place(x=1075, y=180)
+    tk.Entry(modify_groups_screen, textvariable=new_grp, width=30).place(x=1050, y=210)
+    tk.Button(modify_groups_screen, text='Add Group').place(x=1075, y=240)
+
+    tk.Label(modify_groups_screen, text="Add New Type to Selcted Group", bg="white").place(x=1075, y=280)
+    tk.Entry(modify_groups_screen, textvariable=new_type, width=30).place(x=1050, y=310)
+    tk.Button(modify_groups_screen, text='Add Type').place(x=1075, y=360)
+
+    visible = modify_groups_screen
+    modify_groups_screen.place(x=0, y=0, width=1366, height=768)
+
 def get_setting(config, section):
     #Returns a value
     value = config.get(section, "types")
     return value
 
 
-def Get_Types(config, group):
+def sel(event):
+    global type
+    widget = event.widget
+    selection = widget.curselection()
+    type = widget.get(selection[0]).strip()
+
+
+def Get_Types(event):
+    widget = event.widget
+    selection = widget.curselection()
+    group = widget.get(selection[0]).strip()
+    config = configparser.ConfigParser()
+    path = "Config.ini"
+    config.read(path)
     types = get_setting(config, group).split(", ")
-    print(types)
+    typesbox = tk.Listbox(visible)
+    for type in types:
+        typesbox.insert(tk.END, type)
+    typesbox.bind("<Double-Button-1>", sel)
+    typesbox.place(x=490,y=120,width=400,height=550)
 
 
 def Get_Groups():
@@ -24,14 +88,7 @@ def Get_Groups():
     path = "Config.ini"
     config.read(path)
     groups = config.sections()
-    for group in groups:
-        print(group)
-        Get_Types(config, group)
-
-def Add_Groups():
-    global add_groups_screen
-
-    add_groups_screen = tk.Frame(master)
+    return groups
 
 
 def Add():
@@ -39,14 +96,14 @@ def Add():
     nam = (str(name.get())).replace(" ","_")
     dat = str(cal.get())
     qt = str(qty.get())
-    type = str(tkvar.get())
+    typ = type
     if nam != "" and qt != "":
         with open('inventory.db', 'a') as db:
             datetoday = datetime.now()
             datetoday = str(datetoday.strftime("%x"))
-            statement = nam + " " + type + " " + datetoday + " " + dat + " " + qt + " " + qt +"\n"
+            statement = nam + " " + typ + " " + datetoday + " " + dat + " " + qt + " " + qt +"\n"
             db.write(statement)
-            log_statement = "Inserted: " + nam + " " + type + " " + dat + " " + qt + " " + time.ctime() + "\n"
+            log_statement = "Inserted: " + nam + " " + typ + " " + dat + " " + qt + " " + time.ctime() + "\n"
             log.write(log_statement)
             popupmsg("Written to file")
     else:
@@ -131,6 +188,7 @@ def Add_Inventory():
     global type
     global cal
     global qty
+    global place_holder
     global visible
 
     add_inventory_screen = tk.Frame(master, bg = "white")
@@ -144,10 +202,16 @@ def Add_Inventory():
     tk.Label(add_inventory_screen, text="Add to Inventory", bg="white", font=("Calibri", 25), borderwidth=5, relief="solid").place(x=120,y=10,width=1126,height=60)
 
     tk.Label(add_inventory_screen, text="Groups", bg="white", font=("Calibri", 25)).place(x=160,y=75,width=120,height=50)
-    tk.Label(add_inventory_screen, text="Groups", bg="white", borderwidth=5, relief="solid").place(x=40,y=120,width=400,height=550)
+    groupsbox = tk.Listbox(add_inventory_screen)
+    groups = Get_Groups()
+    for group in groups:
+        groupsbox.insert(tk.END, group)
+    groupsbox.bind("<Double-Button-1>", Get_Types)
+    groupsbox.place(x=40,y=120,width=400,height=550)
 
     tk.Label(add_inventory_screen, text="Types", bg="white", font=("Calibri", 25)).place(x=620,y=75,width=100,height=50)
-    tk.Label(add_inventory_screen, text="Types", bg="white", borderwidth=5, relief="solid").place(x=490,y=120,width=400,height=550)
+    place_holder = tk.Label(add_inventory_screen, text="Please Choose a group first.", bg="white", borderwidth=5, relief="solid")
+    place_holder.place(x=490,y=120,width=400,height=550)
 
     tk.Label(add_inventory_screen, text="Name", bg="white").place(x=950, y=310)
     tk.Entry(add_inventory_screen, textvariable=name,width=30).place(x=1050, y=310)
@@ -169,16 +233,15 @@ master = tk.Tk()
 master.title("Inventory Management")
 master.geometry("1366x768")
 
-#Add_Inventory()
+Add_Inventory()
 
 menubar = tk.Menu(master)
 filemenu = tk.Menu(menubar, tearoff=0)
-filemenu.add_command(label="Add Group")
-filemenu.add_command(label="Edit Group")
-filemenu.add_command(label="Delete Group")
+filemenu.add_command(label="Add to Inventory", command=toAdd_Inventory)
+filemenu.add_command(label="Modify Groups", command=toModify_Groups)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=master.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 master.config(menu=menubar)
-#master.mainloop()
-Get_Groups()
+
+master.mainloop()
